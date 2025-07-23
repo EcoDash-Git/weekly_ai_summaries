969fb75878d865001ce519ada7773d85bb22b374
EcoDash-Git <ryan@arweave.org>
2025-07-23 19:48:52 -0400

    Update generate_report.R

--- generate_report.R
+++ generate_report.R
@@ -95,12 +95,48 @@ con <- DBI::dbConnect(
 twitter_raw <- DBI::dbReadTable(con, "twitter_raw") |> as_tibble()
 
 # ── UPDATED: full list of account → canonical‑ID mappings ───────────────────
-main_ids <- tribble(
+main_ids <- tibble::tribble(
   ~username,            ~main_id,
   "weave_db",           "1206153294680403968",
-  # … (same table as before) …
+  "OdyseeTeam",         "1280241715987660801",
+  "ardriveapp",         "1293193263579635712",
+  "redstone_defi",      "1294053547630362630",
+  "everpay_io",         "1334504432973848577",
+  "decentlandlabs",     "1352388512788656136",
+  "KYVENetwork",        "136377177683878784",
+  "onlyarweave",        "1393171138436534272",
+  "ar_io_network",      "1468980765211955205",
+  "Permaswap",          "1496714415231717380",
+  "communitylabs",      "1548502833401516032",
+  "usewander",          "1559946771115163651",
+  "apus_network",       "1569621659468054528",
+  "fwdresearch",        "1573616135651545088",
+  "perma_dao",          "1595075970309857280",
+  "Copus_io",           "1610731228130312194",
+  "basejumpxyz",        "1612781645588742145",
+  "AnyoneFDN",          "1626376419268784130",
+  "arweaveindia",       "1670147900033343489",
+  "useload",            "1734941279379759105",
+  "protocolland",       "1737805485326401536",
+  "aoTheComputer",      "1750584639385939968",
+  "ArweaveOasis",       "1750723327315030016",
+  "aox_xyz",            "1751903735318720512",
+  "astrousd",           "1761104764899606528",
+  "PerplexFi",          "1775862139980226560",
+  "autonomous_af",      "1777500373378322432",
+  "Liquid_Ops",         "1795772412396507136",
+  "ar_aostore",         "1797632049202794496",
+  "FusionFiPro",        "1865790600462921728",
+  "vela_ventures",      "1869466343000444928",
+  "beaconwallet",       "1879152602681585664",
+  "VentoSwap",          "1889714966321893376",
+  "permawebjournal",    "1901592191065300993",
+  "Botega_AF",          "1902521779161292800",
+  "samecwilliams",      "409642632",
+  "TateBerenbaum",      "801518825690824707",
   "ArweaveEco",         "892752981736779776"
 )
+
 
 # 4 ── PRE‑PROCESS TWEETS ----------------------------------------------------
 tweets <- twitter_raw |>
